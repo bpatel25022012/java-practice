27355ae4cace283523a9a25e305006954f96de69
bpatel25022012 <bpatel2502012@gmail.com>
2020-02-08 16:17:09 +0000

    change

---
+++ src/BioData.java
@@ -0,0 +1,10 @@
+public class BioData {
+    public static void main (String[]args){
+
+        System.out.println("Name = Bhavesh ");
+        System.out.println("Surname = Patel");
+        System.out.println("Address = 408,Whitton Avenue East ");
+        System.out.println("Greenford");
+        System.out.println("UB6 0JS");
+    }
+}
